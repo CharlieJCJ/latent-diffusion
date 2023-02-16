b726d5d12590b3eac7dc5b43b79466195f3cdf51
CharlieJCJ <charliechengjieji@berkeley.edu>
2023-02-15 16:25:27 -0800

    a

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -1426,7 +1426,7 @@ class DiffusionWrapper(pl.LightningModule):
         self.diffusion_model = instantiate_from_config(diff_model_config)
         self.conditioning_key = conditioning_key
         assert self.conditioning_key in [None, 'concat', 'crossattn', 'hybrid', 'adm']
-        print("Diffusion model", self.diffusion_model.summary())
+        print("Diffusion model", self.diffusion_model)
 
     def forward(self, x, t, c_concat: list = None, c_crossattn: list = None):
         if self.conditioning_key is None:
